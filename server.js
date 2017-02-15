79a514f2af188b03103aaee5acaef410c5060530
Kim Kwanka <kimkwanka@gmail.com>
2017-02-15 15:33:58 +0100

    Made port variable aware of process.env.PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const stylus = require('stylus');
 
 const app = express();
 
-const port = 80;
+const port = process.env.PORT || 8080;
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
